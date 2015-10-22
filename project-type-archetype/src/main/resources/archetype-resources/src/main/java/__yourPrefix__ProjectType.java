3041f7a9003f7b873702d78c3dbce0dba2b987ef
Stéphane Tournié <stephane.tournie@serli.com>
2015-10-22 09:39:36 +0200

    correcting default runner in ProjectType

--- project-type-archetype/src/main/resources/archetype-resources/src/main/java/__yourPrefix__ProjectType.java
+++ project-type-archetype/src/main/resources/archetype-resources/src/main/java/__yourPrefix__ProjectType.java
@@ -18,15 +18,24 @@ import org.eclipse.che.ide.Constants;
 
 import com.google.inject.Singleton;
 
+import java.util.Arrays;
+
 import static ${package}.${yourPrefix}Attributes.${yourPrefix}_PROJECT_TYPE_ID;
 import static ${package}.${yourPrefix}Attributes.${yourPrefix}_PROJECT_TYPE_NAME;
 import static ${package}.${yourPrefix}Attributes.PROGRAMMING_LANGUAGE;
+
+import static org.eclipse.che.ide.api.project.type.RunnerCategory.JAVA;
 
 @Singleton
 public class ${yourPrefix}ProjectType extends ProjectType {
 
     public ${yourPrefix}ProjectType() {
         super(${yourPrefix}_PROJECT_TYPE_ID, ${yourPrefix}_PROJECT_TYPE_NAME, true, false);
+
         addConstantDefinition(Constants.LANGUAGE, "language", PROGRAMMING_LANGUAGE);
+
+        setDefaultBuilder("maven");
+        setDefaultRunner("system:/java/standalone/simple/cli");
+        addRunnerCategories(Arrays.asList(JAVA.toString()));
     }
 }
