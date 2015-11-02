71526350d8b5f55cde93d5ea7f03e062546189b5
Stéphane Tournié <stephane.tournie@serli.com>
2015-11-02 14:58:21 +0100

    adding multi module archetype

---
+++ multi-module-extension-archetype/src/main/resources/archetype-resources/__yourPrefix__-theme/src/main/java/__yourPrefix__Extension.java
@@ -0,0 +1,35 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package ${package};
+
+import org.eclipse.che.ide.api.extension.Extension;
+import org.eclipse.che.ide.api.parts.PartStackType;
+import org.eclipse.che.ide.api.parts.WorkspaceAgent;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * @author Evgen Vidolob
+ */
+@Singleton
+@Extension(title = "Theme ${package}", version = "1.0.0")
+public class ${yourPrefix}Extension {
+
+    @Inject
+    public ${yourPrefix}Extension(WorkspaceAgent workspaceAgent,
+        ${yourPrefix}Presenter ${yourPrefix}Presenter) {
+        workspaceAgent.openPart(${yourPrefix}Presenter, PartStackType.EDITING);
+    }
+}
